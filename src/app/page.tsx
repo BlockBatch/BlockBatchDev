623c11a9bb21988d116c4b55439ec3ff18766b8f
bigchills <yemi7164@gmail.com>
2025-03-22 01:40:15 +0100

    feat:implement comparison section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import ComparisonSection from "./components/ComparisonSection";
 import FinalCtaSection from "./components/FinalCtaSection";
 
 export default function Home() {
   return (
     <div>
+      <ComparisonSection />
       <FinalCtaSection />
     </div>
   );
