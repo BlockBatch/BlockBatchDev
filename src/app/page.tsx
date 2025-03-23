0fbfafa6653349ec616a6ba37cb45e090bddb8e5
chiscookeke11 <chiscookeke11@gmail.com>
2025-03-23 16:00:14 +0100

    payment operations section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import FeaturesOverviewSection from "./components/FeaturesOverviewSection";
+
 
 export default function Home() {
   return (
     <div>
-      Home
+
+<FeaturesOverviewSection />
     </div>
   );
 }
